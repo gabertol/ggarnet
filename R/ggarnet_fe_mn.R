6833416545858f3625878ee007c93ee544f6e8fe
Gabriel Bertolini <gabertol@gmail.com>
2023-03-27 17:14:39 -0300

    changes in functions

--- R/ggarnet_fe_mn.R
+++ R/ggarnet_fe_mn.R
@@ -10,34 +10,9 @@
 
 ggarnet_fe_mn<-function() {
 
-  grad<-tibble::tribble(~"Mg",~"FeMn",~"Ca",~"Label",
-                        0.7,0.3,0,"Type  A",
-                        0.2,0.8,0,"Type  A",
-                        0.2,0.7,0.1,"Type  A",
-                        0.5,0.4,0.1,"Type  A",
-                        0,1,0,"Type B I",
-                        0,0.9,0.1,"Type B I",
-                        0.2,0.7,0.1,"Type B I",
-                        0.2,0.8,0,"Type B I",
-                        0,0.9,0.1,"Type B II",
-                        0,0.45,0.55,"Type B II",
-                        0.1,0.4,0.5,"Type B II",
-                        0.1,0.6,0.3,"Type B II",
-                        0.2,0.7,0.1,"Type B II",
-                        0.1,0.6,0.3,"Type C I",
-                        0.1,0,0.9,"Type C I",
-                        0.4,0,0.6,"Type C I",
-                        0.4,0.5,0.1,"Type C I",
-                        0.2,0.7,0.1,"Type C I",
-                        1,0,0,"Type C II",
-                        0.7,0.3,0,"Type C II",
-                        0.5,0.4,0.1,"Type C II",
-                        0.4,0.5,0.1,"Type C II",
-                        0.4,0,0.6,"Type C II",
-                        0,0,1,"Type D",
-                        0.1,0,0.9,"Type D",
-                        0.1,0.4,0.5,"Type D",
-                        0,0.45,0.55,"Type D")
+
+  grad<-read.csv("./data/garnet_mg_fe_mn_ca.csv") %>%
+    tibble()
 
 
                        grad.lab<-plyr::ddply(grad,"Label",function(df)
